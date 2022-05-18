cf3fd7de6a4f6499097a1b24204e900d3a6272e9
cyclopes82 <cyclopes82@gmail.com>
2022-05-18 15:31:42 +0530

    macro

--- dbt_demo/models/example/snapshot_source.sql
+++ dbt_demo/models/example/snapshot_source.sql
@@ -2,9 +2,9 @@
 
 with source_data as (
 
-    select 1 as id, 'NJ' as state, '2020-02-01 00:01:00.000'::timestamp as updated_at
+    select 1 as id, 'MH' as state, '2022-02-01 00:01:00.000'::timestamp as updated_at
     union all
-    select null as id, 'CT' as state, '2020-01-01 00:00:00.000'::timestamp as updated_at
+    select null as id, 'MH' as state, '2022-01-01 00:00:00.000'::timestamp as updated_at
     union all
     select 4 as id, 'VT' as state, '2020-01-01 00:00:00.000'::timestamp as updated_at
 
